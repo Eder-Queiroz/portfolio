01bc2a51651369bcd0a7ec6d18d8bfa8024fee17
Eder Queiroz <eder.queiroz@graodireto.com.br>
2026-07-19 14:46:12 -0300

    feat: bento design tokens, base styles and grid

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
+import '@fontsource-variable/inter'
+import '@fontsource-variable/jetbrains-mono'
+import '../styles/tokens.css'
+import '../styles/base.css'
+import '../styles/grid.css'
+
 console.log('portfolio scaffold ok')
